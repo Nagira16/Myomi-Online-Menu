174b1e880d6742b79488cba66fcc03d56c5c0253
Nagira16 <tsuji0032@gmail.com>
2026-04-17 00:21:59 -0700

    header-style

--- myomi-online-menu/components/Header.tsx
+++ myomi-online-menu/components/Header.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Header() {
   return (
-    <nav className="bg-white h-20 w-full flex items-center px-8 relative">
-      <div className="flex justify-center w-60 h-7">
+    <nav className="bg-[#8fa3b3] h-20 w-full flex items-center px-8 relative">
+      <Link href="/" className="flex justify-center w-60 h-7 cursor-default">
         <Image
           src="/myomi_logo.webp"
           alt="Myomi Logo"
           width={240}
           height={28}
         />
-      </div>
+      </Link>
       <div className="left-1/2 -translate-x-1/2 absolute">
-        <h3 className="text-black text-[25px] font-bold">Online Menu</h3>
+        <h3 className="text-white text-[25px] font-bold">Online Menu</h3>
       </div>
     </nav>
   );
